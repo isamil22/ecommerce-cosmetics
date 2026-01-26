f8afc83d5805ec5057ca35cf0954794cd3377987
isamil22 <yesofcourse2024@gmail.com>
2026-01-26 22:01:27 +0100

    Fix potential JSON deserialization issue in UpdateQuantityRequest by adding constructors

--- demo/src/main/java/com/example/demo/dto/UpdateQuantityRequest.java
+++ demo/src/main/java/com/example/demo/dto/UpdateQuantityRequest.java
@@ -3,6 +3,8 @@ package com.example.demo.dto;
 import lombok.Data;
 
 @Data
+@lombok.NoArgsConstructor
+@lombok.AllArgsConstructor
 public class UpdateQuantityRequest {
     private Integer quantity;
 }
